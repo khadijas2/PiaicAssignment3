89e1bc5bc74edb1cdb56bc6fa0e416cd69c02857
Khadija Sikander <khadijasikander02@gmail.com>
2023-08-02 23:18:42 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// // LOWERCASE
+let Fname = "Khadija Sikander";
+console.log(Fname.toLowerCase());
+// // UPPERCASE
+let vaL1 = "khadija sikander";
+console.log(vaL1.toUpperCase());
+// // TITLECASE
+let vaL = "khadija";
+console.log(vaL[0].toUpperCase() + vaL.slice(1));
